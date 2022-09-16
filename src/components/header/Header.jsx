fcc0cf456cd2187a22e0410c57ef8b943858ef97
Nicoll Oliver <nicolldon96@gmail.com>
2022-09-15 22:28:22 -0400

    components add in

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import CTA from './CTA';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <Header>
+      <div className="container header__container ">
+        <h5>Hello I'm</h5>
+        <h1> Nicoll Oliver</h1>
+        <h5 className="text-light">Fullstack Developer | Designer | Drafter | Dreamer </h5>
+        <CTA />
+      </div>
+
+    </Header>
+  )
+}
+
+export default Header;
