5d87bca32c4bfcfd99cfb565796b29912f10ace8
Nicoll Oliver <nicolldon96@gmail.com>
2022-09-20 14:55:12 -0400

    header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,17 +1,27 @@
+import React from 'react';
 import CTA from './CTA';
+import ME from '../../assets/nicoll/16 color nicoll.jpg'
+import HeaderSocials from './HeaderSocials';
 import './Header.css';
 
 const Header = () => {
 
   return (
-    <Header>
+    <header>
       <div className="container header__container ">
         <h5>Hello I'm</h5>
         <h1> Nicoll Oliver</h1>
         <h5 className="text-light">Fullstack Developer | Designer | Drafter | Dreamer </h5>
-        {/* <CTA /> */}
+        <CTA/>
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+        <HeaderSocials/>
       </div>
-    </Header>
+    </header>
   )
 }
 
