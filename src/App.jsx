59668874f6f8f8456ba2ae96f4266f24f14c5ceb
Nicoll Oliver <nicolldon96@gmail.com>
2022-09-21 20:06:29 -0400

    add in more components to display

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 // import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Header from './components/header/Header'
-// import About from './pages/about/About'
-// import Experience from './components/experience/Experience'
+import Nav from "./components/nav/Nav"
+
+import About from './pages/about/About'
+import Experience from './components/experience/Experience'
 // import NotFound from './pages/NotFound/NotFound'
-// import Services from './components/services/Services'
-// import Portfolio from './components/portfolio/Portfolio'
-// import Testimonials from './components/testimonials/Testimonials'
-// import Contact from './pages/contact/Contact'
-// import Footer from './components/footer/Footer'
-// import Nav from "./components/nav/Nav"
+import Services from './components/services/Services'
+import Portfolio from './components/portfolio/Portfolio'
+import Testimonials from './components/testimonials/Testimonials'
+import Contact from './pages/contact/Contact'
+import Footer from './components/footer/Footer'
 
 
 const App = () => {
   return (
    <>
       <Header/>
+      <Nav/>
+      <About/>
+      <Experience/>
+      <Services/>
+      <Portfolio/>
+      <Testimonials/>
+      <Contact/>
+      <Footer/>
    </> 
   )
 }
