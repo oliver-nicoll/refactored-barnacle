1dbfc20e1b3423281feda7214857a75c7f105732
Nicoll Oliver <nicolldon96@gmail.com>
2022-09-21 20:08:11 -0400

    component about build

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,9 +1,48 @@
 import React from 'react';
+import Me from '../../assets/mee.png'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
+
 import './About.css';
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+      <h5>
+        Get To Know
+      </h5>
+      <h2>About Me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={Me} alt="About Image" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>5+ Years Designing</small>
+            </article>
+            <article className='about__card'>
+              <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>100+ Clients</small>
+            </article>
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>80+ Completed Projects</small>
+            </article>
+          </div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum molestias repudiandae nostrum, est debitis dolorum nam officiis voluptas, deserunt, ipsum culpa dolorem minus mollitia nulla alias. Eveniet laboriosam sapiente tenetur!</p>
+          <a href="#contact" className='btn'>Let's Talk</a>
+        </div>
+      </div>
+
+    </section>
   )
 }
 
