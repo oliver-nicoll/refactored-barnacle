94052ef9946c0d3768f0d329798d0bda2f055d27
Nicoll Oliver <nicolldon96@gmail.com>
2022-09-23 00:32:03 -0400

    portfolio start build

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+// import IMG1 from
 import './Portfolio.css';
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section >
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className='portfolio__item'> 
+          <div className="portfolio__item-image"></div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com" className='btn' target="_blank">Github</a>
+          <a href="https://dribble.com" className='btn btn-primary' target='_blank'>Live Demo</a>
+        </article>
+      </div>
+    </section >
     
   )
 }
