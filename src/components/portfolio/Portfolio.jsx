4ffde66d49ea5782bfaddca51bb99dab6ef8bc3e
Nicoll Oliver <nicolldon96@gmail.com>
2022-09-24 16:12:24 -0400

    build out final components, fix email js setup

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,6 +4,30 @@ import IMG2 from '../../assets/project_2.png'
 import IMG3 from '../../assets/project_3.png'
 import './Portfolio.css';
 
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Tourney',
+    github: 'https://github.com/oliver-nicoll/tourney-maker.git',
+    demo: 'https://youtu.be/QXVDVYwgsjU'
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: "A Perfect Blend",
+    github: 'https://github.com/oliver-nicoll/A-Perfect-Blend.git',
+    demo: 'https://youtu.be/uHyBUd3CqUU'
+  },
+  {
+    id:3, 
+    image: IMG3, 
+    title: 'Investor',
+    github: 'https://github.com/oliver-nicoll/Sinatra-Investor_Property_Tracker.git',
+    demo: 'https://www.youtube.com/watch?v=FkUdx7uedaI'
+  }
+]
+
 const Portfolio = () => {
   return (
     <section id='portfolio'>
@@ -12,16 +36,20 @@ const Portfolio = () => {
 
       <div className="container portfolio__container">
        { 
-        <article className='portfolio__item'> 
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>Tourney</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com" className='btn' target="_blank">Github</a>
-            <a href="https://dribble.com" className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
+        data.map (({id, image, title, github, demo}) => {
+          return (
+            <article key={id} className='portfolio__item'> 
+            <div className="portfolio__item-image">
+              <img src={image} alt={title} />
+            </div>
+            <h3>{title}</h3>
+            <div className="portfolio__item-cta">
+              <a href={github} className='btn' target="_blank">Github</a>
+              <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+            </div>
+          </article>
+          )
+        })
         }
       </div>
     </section >
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,71 @@
 import React from 'react';
+import AVA1 from '../../assets/avatar1.jpg'
+import AVA2 from '../../assets/avatar2.jpg'
+import AVA3 from '../../assets/avatar3.jpg'
+import AVA4 from '../../assets/avatar4.jpg'
+
+import { Pagination } from 'swiper';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+
 import './Testimonials.css';
+
+const testData = [
+  {
+    id: 1,
+    image: AVA1,
+    name: 'Lorem Ipsum',
+    testimonial: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit voluptatibus atque, maxime optio tenetur nobis.' 
+  },
+  {
+    id: 2,
+    image: AVA2,
+    name: 'Lorem Ipsum',
+    testimonial: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit voluptatibus atque, maxime optio tenetur nobis.' 
+  },
+  {
+    id: 3,
+    image: AVA3,
+    name: 'Lorem Ipsum',
+    testimonial: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit voluptatibus atque, maxime optio tenetur nobis.' 
+  },
+  {
+    id: 4,
+    image: AVA4,
+    name: 'Lorem Ipsum',
+    testimonial: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit voluptatibus atque, maxime optio tenetur nobis.' 
+  }
+]
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section >
+    <section id='testimonials'>
+      <h5>Review From Clients</h5>
+      <h2>Testimonials</h2>
+
+      <Swiper className="container testimonials__container"
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}>
+        {
+          testData.map(({id, image, name, testimonial}) => {
+            return (
+              <SwiperSlide key={id} className='testimonial'>
+                <div className="client__avatar">
+                  <img src={image} alt={name} />
+                </div>
+                  <h5 className='client__name'>{name}</h5>
+                  <small className='client__review'>
+                    {testimonial}
+                  </small>
+              </SwiperSlide>
+            )
+        })}
+      </Swiper>
+    </section >
 
   )
 }
