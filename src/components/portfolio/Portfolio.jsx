aa151c0737175cc77756006b31f2ebe8cfed5afa
Nicoll Oliver <nicolldon96@gmail.com>
2022-09-23 23:12:07 -0400

    component build

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-// import IMG1 from
+import IMG1 from '../../assets/project_1.png'
+import IMG2 from '../../assets/project_2.png'
+import IMG3 from '../../assets/project_3.png'
 import './Portfolio.css';
 
 const Portfolio = () => {
@@ -9,12 +11,18 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
+       { 
         <article className='portfolio__item'> 
-          <div className="portfolio__item-image"></div>
-          <h3>This is a portfolio item title</h3>
-          <a href="https://github.com" className='btn' target="_blank">Github</a>
-          <a href="https://dribble.com" className='btn btn-primary' target='_blank'>Live Demo</a>
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>Tourney</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com" className='btn' target="_blank">Github</a>
+            <a href="https://dribble.com" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
         </article>
+        }
       </div>
     </section >
     
