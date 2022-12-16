4b84449cfcdce8d78567d8059481dfe5dd8129eb
Nicoll Oliver <78064156+oliver-nicoll@users.noreply.github.com>
2022-12-15 23:44:12 -0500

    footer word change

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -22,7 +22,7 @@ const Footer = () => {
         <li><a href="#about">About</a></li>
         <li><a href="#experience">Experience</a></li>
         <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#testimonials">Projects</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
 
