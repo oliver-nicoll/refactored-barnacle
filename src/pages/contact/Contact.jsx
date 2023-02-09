7b0e2cc81eae6b204d161a5dcb4cccc150f0e41e
Nicoll Oliver <78064156+oliver-nicoll@users.noreply.github.com>
2023-02-09 12:41:51 -0500

    sensitive info fix

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -37,19 +37,19 @@ const Contact = () => {
           <article className='contact__option'>
             <MdOutlineEmail className='contact__option-icon'/>
             <h4>Email</h4>
-            <h5>nicoll.oliver.business@gmail.com</h5>
+            {/* <h5>nicoll.oliver.business@gmail.com</h5> */}
             <a href="mailto:nicoll.oliver.business@gmail.com" target="_blank">Send a message</a>
           </article>
           <article className='contact__option'>
             <RiMessengerLine className='contact__option-icon'/>
             <h4>Messenger</h4>
-            <h5>Nicoll Oliver</h5>
+            {/* <h5>Nicoll Oliver</h5> */}
             <a href="https://m.me/nicolldon/" target="_blank">Send a message</a>
           </article>
           <article className='contact__option'>
             <BsWhatsapp className='contact__option-icon'/>
             <h4>WhatsApp</h4>
-            <h5>+5708548313</h5>
+            {/* <h5>+5708548313</h5> */}
             <a href="https://api.whatsapp.com/send?phone=+15708548313" target="_blank">Send a message</a>
           </article>
         </div>
